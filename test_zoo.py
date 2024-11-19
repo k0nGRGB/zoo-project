d839b8597b742d9f06e62ae7b25e8e88bd987212
K0nG (-_-) <132033590+k0nGRGB@users.noreply.github.com>
2024-11-19 08:17:19 +0000

    Added the test case.

--- test_zoo.py
+++ test_zoo.py
@@ -6,9 +6,21 @@ class TestZoo(unittest.TestCase):
         self.zoo = Zoo()
 
     def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
+        self.assertEqual(self.zoo.get_ticket_price(1), 50 )
        
     # Add your additional test cases here.
+        
+    def test_age_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), 'Age cannot less than zero')
+
+    def test_teen_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+    
+    def test_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+    
+    def test_elderly_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
 
 if __name__ == '__main__':
     unittest.main()
